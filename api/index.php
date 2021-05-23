aa67c4294eaf2b19ad72bfbc96b71fd4a84e923b
yaozm <ityaozm@gmail.com>
2021-05-23 18:12:36 +0800

    Add vercel config files

---
+++ api/index.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the coolephp/skeleton.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+require __DIR__.'/../public/index.php';
